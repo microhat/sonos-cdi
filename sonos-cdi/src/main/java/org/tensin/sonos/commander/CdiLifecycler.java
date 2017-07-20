56854db105be7040386b9bc69b1ce4d328d90c7d
Fabian Henniges <fabian.henniges@economore.de>
2017-07-20 17:12:35 +0200

    Add CDI specifics

---
+++ sonos-cdi/src/main/java/org/tensin/sonos/commander/CdiLifecycler.java
@@ -0,0 +1,35 @@
+package org.tensin.sonos.commander;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tensin.sonos.SonosException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class CdiLifecycler {
+
+    private Logger log = LoggerFactory.getLogger(CdiLifecycler.class);
+    private DaemonController controller;
+
+    private void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        controller = DaemonController.createController();
+        try {
+            controller.start();
+        } catch (SonosException e) {
+            log.error(e.getMessage(),e);
+        }
+    }
+
+    private void destroy(@Observes @Destroyed(ApplicationScoped.class) Object init) {
+        try {
+            controller.stop();
+        } catch (SonosException e) {
+            log.error(e.getMessage(),e);
+        }
+    }
+
+}
